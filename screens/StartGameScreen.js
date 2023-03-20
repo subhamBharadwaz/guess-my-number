4e010ce14675b3f81cbe94b7781e189c9eac9dbe
Subham Shyamal Bharadwaz <subhamsbharadwaz@gmail.com>
2023-03-20 19:49:10 +0530

    init: :tada: Initial Setup

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
